98f8955baf8432242c23c62ada30437b20f6d8a9
Brian Whitton <me@brianwhitton.com>
2016-11-18 18:13:26 -0500

    import siteId as set in config

--- app/story/route.js
+++ app/story/route.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import service from 'ember-service/inject';
 import PlayParamMixin from 'overhaul/mixins/play-param';
+import config from 'overhaul/config/environment';
 const { get } = Ember;
 const { hash: waitFor } = Ember.RSVP;
 
@@ -56,14 +57,14 @@ export default Ember.Route.extend(PlayParamMixin, {
       page: `/story/${get(model, 'story.slug')}`,
       title: label,
       nprVals,
-      isNpr: true
     });
     
     // data pipeline
     dataPipeline.reportItemView({
       cms_id: get(model, 'story.id'),
       item_type: get(model, 'story.itemType'),
-      site_id: get(model, 'story.siteId')
+      site_id: get(model, 'story.siteId'),
+      client: config.clientSlug
     });
   }
 });
