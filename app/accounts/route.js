032a86d7dcc012c389699de24441e445b9e1fd20
Brian Whitton <me@brianwhitton.com>
2016-12-11 21:47:39 -0500

    actually retrive the user and add it to the tmeplate

--- app/accounts/route.js
+++ app/accounts/route.js
@@ -1,13 +1,10 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   session: Ember.inject.service('session'),
 
   model() {
-    return this.store.find('user', 'current').then(u => {
-      debugger;
-    })
+    return this.store.find('user', 'current');
   }
 });
