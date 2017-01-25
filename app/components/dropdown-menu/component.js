9091ce4e8f66969e255513385defc5c0cc7d230e
Brian Whitton <me@brianwhitton.com>
2017-01-25 11:03:51 -0500

    WIP dropdown fix

--- app/components/dropdown-menu/component.js
+++ app/components/dropdown-menu/component.js
@@ -1,5 +1,13 @@
 import Component from 'ember-component';
 
 export default Component.extend({
-  classNames: ['dropdown', 'dropdown--br', 'dropdown--nearwhite']
+  classNames: ['dropdown', 'dropdown--br', 'dropdown--nearwhite'],
+  classNameBindings: ['isOpen'],
+  isOpen: false,
+  
+  actions: {
+    toggleDropdown() {
+      this.toggleProperty('isOpen');
+    }
+  }
 });
