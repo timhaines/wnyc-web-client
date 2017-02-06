4d8edf20c6800013a722a706a0e28e8d100d12d7
walsh9 <walsh9@gmail.com>
2017-02-06 13:31:01 -0500

    Collect coverage reports as artifacts in circle.yml

--- config/coverage.js
+++ config/coverage.js
@@ -1,11 +1,5 @@
 /*jshint node:true*/
-function coverageFolder() {
-  var circleArtifactsFolder = process.env['CIRCLE_ARTIFACTS'];
-  return circleArtifactsFolder || 'coverage';
-}
-
 module.exports = {
   coverageEnvVar: 'CIRCLECI',
   useBabelInstrumenter: true,
-  coverageFolder: coverageFolder()
-};
+}
