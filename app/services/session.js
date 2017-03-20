780b70788d2d6e7d25bf4144c5300ffd161ffbf4
walsh9 <walsh9@gmail.com>
2017-03-20 12:48:36 -0400

    display flash messages in account-wrapper template

--- app/services/session.js
+++ app/services/session.js
@@ -22,7 +22,7 @@ export default SessionService.extend({
         return RSVP.Promise.resolve(null);
       }
     }
-    
+
     let { browserId } = this.get('data');
     if (legacyId || browserId) {
       if (report) {
@@ -35,7 +35,7 @@ export default SessionService.extend({
         .then( ({ browser_id }) => this.set('data.browserId', browser_id));
     }
   },
-  
+
   staffAuth() {
     fetch(`${config.wnycAdminRoot}/api/v1/is_logged_in/?bust_cache=${Math.random()}`, {
       credentials: 'include'
